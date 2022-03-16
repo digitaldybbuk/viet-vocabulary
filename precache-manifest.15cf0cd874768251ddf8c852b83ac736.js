3e704f700def6c213d60b5e2e68edbfd68f3806b
Manuel Castillo <manuel.franklin.castillo@gmail.com>
2022-03-16 01:40:34 -0500

    Updates

---
+++ precache-manifest.15cf0cd874768251ddf8c852b83ac736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4f561d42db557c7d69e1162195312b",
+    "url": "/viet-vocabulary/index.html"
+  },
+  {
+    "revision": "7103e56edcd35b38e707",
+    "url": "/viet-vocabulary/static/css/main.6b716f48.chunk.css"
+  },
+  {
+    "revision": "d7bb63d1c2823af01013",
+    "url": "/viet-vocabulary/static/js/2.c98a9cd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/viet-vocabulary/static/js/2.c98a9cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7103e56edcd35b38e707",
+    "url": "/viet-vocabulary/static/js/main.ae66df5f.chunk.js"
+  },
+  {
+    "revision": "3ca83200a78429488ce5",
+    "url": "/viet-vocabulary/static/js/runtime-main.180d7d74.js"
+  }
+]);
